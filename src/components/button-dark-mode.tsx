6ea234fbebb0399a6732d787bcb36a5b1281ba4a
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-12-15 08:30:53 -0300

    fix: render client

--- src/components/button-dark-mode.tsx
+++ src/components/button-dark-mode.tsx
@@ -1,16 +1,25 @@
 'use client'
 
-import { ComponentProps } from 'react'
-import { twMerge } from 'tailwind-merge'
 import { motion } from 'framer-motion'
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { ComponentProps, useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export function ButtonDarkMode({ className }: ComponentProps<'button'>) {
+  const [isLoaded, setIsLoaded] = useState(false)
   const { theme, setTheme } = useTheme()
 
   const icon = theme === 'dark' ? <Sun /> : <Moon />
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
+  useEffect(() => {
+    setIsLoaded(true)
+  }, [])
+
+  if (!isLoaded) {
+    return null
+  }
 
   return (
     <motion.button
